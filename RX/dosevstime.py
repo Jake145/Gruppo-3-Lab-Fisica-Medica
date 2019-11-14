424e3efc900169718bf68cd60184252d0af3f969
Jake Harold Pensavalle <jakeharold.pensavalle@gmail.com>
2019-11-14 15:06:51 +0200

    added hvl code for 100kVp

--- RX/dosevstime.py
+++ RX/dosevstime.py
@@ -1,41 +1,34 @@
 import numpy as np
-import scipy
-import matplotlib.pyplot as plt
+from scipy.stats import*
 import pylab
-from scipy.optimize import curve_fit
-from scipy import stats
-
 
 y1=np.linspace(0,1100,1000)    #genero una ascissa a caso per il fit
 dose,time=pylab.loadtxt('dosevstime.txt',unpack=True) 
 Ds=0.03*dose 
-def f1(x,m,q):
-
-    y=m*x+q
+def mean(y):
+    z=sum(y)/len(y)
+    return z
     
-    return y 
-     
 
-     
-popt, pcov= curve_fit(f1, time, dose, (0.,0.),Ds,absolute_sigma=False)
-DOF=len(time)-3
-chi2_1 = sum(((f1(time,*popt)-dose)/Ds)**2)
-dm,dq= np.sqrt(pcov.diagonal())
-chi2_1redux=chi2_1/DOF
+slope,intercept,r_value,pvalue,std_err=linregress(time,dose)
+print('Per il fit lineare coefficiente angolare è %.3f, la intercetta è %.3f, R value è %.3f, p value è %.3f, la deviazione standard è %.3f, la media è %.3f' % (slope,intercept,r_value,pvalue,std_err,mean(dose)))
+p1=np.polyfit(time,dose,1)
+p2=np.polyfit(time,dose,2)
 
-m=popt[0]
-q=popt[1]
-print(*popt)
 
-print(pcov)
 
-pvalue=1 - stats.chi2.cdf(chi2_1, DOF)
-print('il coefficiente angolare è %.3f, la intercetta è %.3f' % (m,q))
-print('il chi2 è=%.3f, i DOF sono=%.3f' % (chi2_1, DOF))
-print('il chi2 ridotto è=%.3f '% (chi2_1redux))
-print('il pvalue è=%.3f'% (pvalue))
 
+
+
+##calcolo r^2
+yfit=p2[0]*time**2+p2[1]*time + p2[2]
+yres=dose-yfit
+SSresid=sum(pow(yres,2))
+SStotal=len(dose)*np.var(dose)
+rsq=1-SSresid/SStotal
+print('Per il fit quadratico il coefficiente del secondo grado è %.3f, del primo è %.3f, del termine costante è %.3f, R value è %.3f' % (p2[0],p2[1],p2[2],rsq))
 ##plot 
+
 pylab.figure('calibrazione')
 
 
@@ -46,7 +39,9 @@ pylab.ylabel('dose [muGy]')
 
 
 pylab.title('Dose vs Time')
-pylab.plot(y1,f1(y1,*popt), color='green', label="fit")
+pylab.plot(y1,np.polyval(p1,y1), 'g-', label="linear fit")
+pylab.plot(y1, np.polyval(p2,y1),'r--',label="quadratic fit")
+pylab.legend()
 pylab.grid()
 
 
---
+++ RX/hvl.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.stats import*
+import pylab
+
+y1=np.linspace(0,10,1000) 
+
+#hvl 100 kvp
+
+dose,distance=np.loadtxt('hvl100kvp.txt',unpack=True)
+rel_I=dose/max(dose)
+ds=0.03*rel_I
+
+p1=np.polyfit(distance,rel_I,1)
+yfit1=p1[0]*distance+p1[1]
+yres1=rel_I-yfit1
+SSresid1=sum(pow(yres1,2))
+SStotal1=len(rel_I)*np.var(rel_I)
+rsq1=1-SSresid1/SStotal1
+print('Per il fit lineare il coefficiente del primo grado è %.3f, del termine costante è %.3f, R value è %.3f' % (p1[0],p1[1],rsq1))
+
+p2=np.polyfit(distance,rel_I,2)
+yfit2=p2[0]*distance**2+p2[1]*distance +p2[2]
+yres2=rel_I-yfit2
+SSresid2=sum(pow(yres2,2))
+SStotal2=len(rel_I)*np.var(rel_I)
+rsq2=1-SSresid2/SStotal2
+print('Per il fit quadratico il coefficiente del secondo grado è %.3f,del primo è %.3f, del termine costante è %.3f, R value è %.3f' % (p2[0],p2[1],p2[2],rsq2))
+
+p3=np.polyfit(distance,rel_I,3)
+yfit3=p3[0]*distance**3+p3[1]*distance**2 + p3[2]*distance +p3[3]
+yres3=rel_I-yfit3
+SSresid3=sum(pow(yres3,2))
+SStotal3=len(rel_I)*np.var(rel_I)
+rsq3=1-SSresid3/SStotal3
+print('Per il fit cubico il coefficiente del terzo grado è %.3f, del second è %.3f,del primo è %.3f, del termine costante è %.3f, R value è %.3f' % (p3[0],p3[1],p3[2],p3[3],rsq3))
+
+pylab.figure('hvl100pvk')
+
+
+pylab.errorbar( distance, rel_I, ds , fmt= '.', ecolor= 'magenta')
+
+pylab.xlabel('distance [mm]')
+pylab.ylabel('relative intensity [%]')
+
+
+pylab.title('Intensity vs Distance at 100 kvp')
+
+pylab.plot(y1, np.polyval(p1,y1),'g--',label="linear fit")
+pylab.plot(y1, np.polyval(p2,y1),'b--',label="square fit")
+pylab.plot(y1, np.polyval(p3,y1),'r--',label="cubic fit")
+pylab.legend()
+pylab.grid()
+
+
+pylab.show()
