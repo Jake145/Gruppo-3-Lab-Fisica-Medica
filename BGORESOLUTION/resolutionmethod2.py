02e72894433c7da49cffa2cc4bf3ca922bd30913
Jake145 <jakeharold.pensavalle@gmail.com>
2020-03-18 15:38:15 +0200

    finished Resolution

--- BGORESOLUTION/resolutionmethod2.py
+++ BGORESOLUTION/resolutionmethod2.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 
 import glob
 bins=200
-
+#cancelliamo file vecchi
 crap1=glob.glob('hist*.png')
 for craps in crap1:
     try:
         remove(craps)
     except:
         pass
+#iniziamo il tutto caricando i dati
 filenames=glob.glob('data*.txt')
 
 
@@ -23,6 +24,7 @@ for i in range(len(filenames)):
     print(f)
 
     data=np.loadtxt(f,unpack=True)
+#qui definisco gli estremi dei fotopicchi
     if i==0:
         a=2.3
         b=10
@@ -54,11 +56,11 @@ for i in range(len(filenames)):
         pass
 
 
-
+#cominciamo la figura
     plt.figure('%s'%f.replace('.txt',''))
     bin_heights, bin_borders, _=plt.hist(data,bins,facecolor='g',ec='black',alpha=0.5,label='histogram data',density=True)
     bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
-
+#qui creo il vettore per il fit sul fotopicco eliminando la parte che non interessa
     newcenters=[]
     newheights=[]
     for j in range(len(bin_heights)):
@@ -69,6 +71,7 @@ for i in range(len(filenames)):
             pass
     y=np.array(newheights)
     x=np.array(newcenters)
+#in base allo spettro fitto una gaussiana esponenzialmente corretta o una skewed
     if i<6 and i!=1:
         mod=ExponentialGaussianModel()
         text='Exponential Gaussian Fit'
@@ -77,14 +80,23 @@ for i in range(len(filenames)):
         text='Skewed Gaussian Fit'
     else:
         print('ERROR')
+#indovina i parametri iniziali
     pars=mod.guess(y,x=x)
-
+#fit
     out = mod.fit(y, pars, x=x)
+#ora calcoliamo le risoluzioni
 
-
+    fwhm=out.params['fwhm'].value
+    center=out.params['center'].value
+    resolution=100*fwhm/center
+# salviamo i risultati del fit su un txt
+    with open('fit_result%s.txt'%f.replace('.txt',''), 'w') as fh:
+        fh.write(out.fit_report())
+#figura
     plt.title('Histogram Resolution of %s '%f.replace('.txt',''))
     plt.xlabel('adc')
     plt.plot(x, out.best_fit, 'r-', label=text)
+    plt.plot([], [], ' ', label='Resolution: %.2f'%resolution)
     plt.grid()
     plt.legend()
     plt.ylabel('frequency')
